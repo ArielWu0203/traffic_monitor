feab1a9691e161a04171b94d2abd05eefaf1624c
ArielWu0203 <lucky020372@gmail.com>
2019-03-24 21:42:33 +0800

    build controller

--- flow_stats.py
+++ flow_stats.py
@@ -1,26 +1,22 @@
-
 from operator import attrgetter
-
 from ryu.app import simple_switch_13
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.lib import hub
 
-## 繼承自SimpleSwitch13
-class SimpleMonitor(simple_switch_13.SimpleSwitch13):
+## from SimpleSwitch13
 
+class SimpleMonitor(simple_switch_13.SimpleSwitch13):
     def __init__(self, *args, **kwargs):
         super(SimpleMonitor, self).__init__(*args, **kwargs)
-        ## 儲存監控中的 datapaths
+        ## store datapaths
         self.datapaths = {}
-        ## 建立threads
+        ## build threads
         ## There are some eventlet wrappers and basic class implementation in ryu.lib.hub.
         ## The thread actually created is an eventlet green thread.
         self.monitor_thread = hub.spawn(self._monitor)
-
-    @set_ev_cls(ofp_event.EventOFPStateChange,
-                [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    @set_ev_cls(ofp_event.EventOFPStateChange,[MAIN_DISPATCHER, DEAD_DISPATCHER])
     def _state_change_handler(self, ev):
         datapath = ev.datapath
         if ev.state == MAIN_DISPATCHER:
@@ -32,30 +28,29 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
                 self.logger.debug('unregister datapath: %016x', datapath.id)
                 del self.datapaths[datapath.id]
 
-    ## 每10秒向switch發出請求，得到統計資訊。
+    ## every 10 secs,send request for switches,in order to get flow statistic.
     def _monitor(self):
         while True:
             for dp in self.datapaths.values():
                 self._request_stats(dp)
             hub.sleep(10)
 
-    ## 定期呼叫
     def _request_stats(self, datapath):
         self.logger.debug('send stats request: %016x', datapath.id)
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         
-        ## OFPFlowStatsRequest : 向 switch 取得所有 flow entry 資料。
+        ## OFPFlowStatsRequest : get the information of all flow entries from switches.
         req = parser.OFPFlowStatsRequest(datapath)
         datapath.send_msg(req)
 
-        ## OFPPortStatsRequest : 向 switch 取得 ports 的資料。
-        ## OFPP_ANY : 取得所有 port 的資料。
+        ## OFPPortStatsRequest : get the information of ports from switches.
+        ## OFPP_ANY : all ports
         req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
         datapath.send_msg(req)
 
     ## EventOFPFlowStatsReply : The switch responds with this message to an individual flow statistics request.
-    ## 建立 event handler，接收 switch 發來的 Reply 訊息。
+    ## event handler : receive Reply messages from switches.
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
         body = ev.msg.body
